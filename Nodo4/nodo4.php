33f1059a5defe0536654ee552abf1e39ced35e04
sergioandresco <43445037+sergioandresco@users.noreply.github.com>
2022-06-17 01:18:44 -0500

    Add files via upload

---
+++ Nodo4/nodo4.php
@@ -0,0 +1,21 @@
+<?php 
+header('Content-Type: application/json');
+if(isset($_GET["JSONData"])){
+    $hola = "Dato almacenado en Bloodhound"; 
+    $array = json_decode($_GET["JSONData"]);
+    $Numero = $array -> Numero;
+    $archivo = fopen($_SERVER["DOCUMENT_ROOT"] . "/Nodo4/txt/NUMERO.txt","wb");
+    fputs($archivo,$Numero);
+    fclose($archivo);
+    echo $hola;   
+}
+if(isset($_POST["Numero"])){
+    $Peticion = $_POST["Numero"];
+    $txtAbrir= fopen($_SERVER["DOCUMENT_ROOT"] . "/Nodo4/txt/NUMERO.txt", 'r');
+    $txtLeer = fgets($txtAbrir);
+    fclose($txtAbrir);
+
+    echo $txtLeer;
+}
+
+?>
